620bad5f6265329387b991657e9b9a18767a4234
Marcus Toftås <toftasm@student.chalmers.se>
2021-01-24 15:14:56 +0100

    Minor Bug Fix

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/MackanT/OctoPrint-Ledcontroller"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['pigpiod']
+plugin_requires = []
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
