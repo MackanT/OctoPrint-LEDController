b65bdcdcc61000d1faf4bdc37a3388df65d2c46c
Marcus Toftås <toftasm@student.chalmers.se>
2021-01-25 13:28:51 +0100

    Potential BugFix

--- octoprint_LEDController/static/js/LEDController.js
+++ octoprint_LEDController/static/js/LEDController.js
@@ -3,15 +3,15 @@ $(function() {
         var self = this;
 
         // listeners to detect changes in the inputted values.
-        var colorInput = document.getElementById("clrPicker");
-        var rInput = document.getElementById("red_str");
-        var gInput = document.getElementById("green_str");
-        var bInput = document.getElementById("blue_str");
+        var color_input = document.getElementById("hex_str");
+        var r_input = document.getElementById("red_str");
+        var g_input = document.getElementById("green_str");
+        var b_input = document.getElementById("blue_str");
 
-        colorInput.addEventListener("input", updateColorNum);
-        rInput.addEventListener("change", updateColorWheel);
-        gInput.addEventListener("change", updateColorWheel);
-        bInput.addEventListener("change", updateColorWheel);
+        color_input.addEventListener("input", update_color_num_input);
+        r_input.addEventListener("change", update_color_wheel_input);
+        g_input.addEventListener("change", update_color_wheel_input);
+        b_input.addEventListener("change", update_color_wheel_input);
 
         self.settings = parameters[0];
 
@@ -21,51 +21,52 @@ $(function() {
         self.b_val = ko.observable();
 
         // Updates numbs when color wheel has been used
-        function updateColorNum() {
-            hex = colorInput.value;
-            self.r_val = hexToRgb(hex).r;
-            self.g_val = hexToRgb(hex).g;
-            self.b_val = hexToRgb(hex).b;
-            self.setColor();
+        function update_color_num_input() {
+            hex = color_input.value;
+            self.r_val = hex_to_rgb(hex).r;
+            self.g_val = hex_to_rgb(hex).g;
+            self.b_val = hex_to_rgb(hex).b;
+            self.set_color();
         }
         
         // Updates colorwheel when numbs have been used
-        function updateColorWheel() {
-            self.r_val = Number(rInput.value);
+        function update_color_wheel_input() {
+            self.r_val = parseInt(r_input.value, 10);
             if (self.r_val > 255){self.r_val = 255}
             if (self.r_val < 0){self.r_val = 0}
-            self.g_val = Number(gInput.value);
+            
+            self.g_val = parseInt(g_input.value, 10);
             if (self.g_val > 255){self.g_val = 255}
             if (self.g_val < 0){self.g_val = 0}
-            self.b_val = Number(bInput.value);
+            
+            self.b_val = parseInt(b_input.value, 10);
             if (self.b_val > 255){self.b_val = 255}
             if (self.b_val < 0){self.b_val = 0}
-            self.setColor();
+
+            self.set_color();
         }
 
-        // this will be called when the user clicks the "Set Color" button
-        self.setColor = function() {
-
+        // called when the user clicks the "Set Color" button
+        self.set_color = function() {
             document.getElementById('red_str').value = self.r_val;
             document.getElementById('green_str').value = self.g_val;
             document.getElementById('blue_str').value = self.b_val;
-            document.getElementById('clrPicker').value = rgbToHex(self.r_val, self.g_val, self.b_val);
-
+            document.getElementById('hex_str').value = rgb_to_hex(self.r_val, self.g_val, self.b_val);
         };
 
         // converts r,g,b to hex value
-        function rgbToHex(r, g, b) {
-            return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+        function rgb_to_hex(r, g, b) {
+            return "#" + component_to_hex(r) + component_to_hex(g) + component_to_hex(b);
           }
         
         // converts individual 0-255 to hex
-        function componentToHex(c) {
+        function component_to_hex(c) {
             var hex = c.toString(16);
             return hex.length == 1 ? "0" + hex : hex;
         }  
 
-        // converts hex to rgb values, call with hexToRgb.? / ? = r,g,b
-        function hexToRgb(hex) {
+        // converts hex to rgb values, call with hex_to_rgb.? // ? = r,g,b
+        function hex_to_rgb(hex) {
             var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
             return result ? {
               r: parseInt(result[1], 16),
@@ -76,16 +77,16 @@ $(function() {
         
         // Initiate setting values with current values
         self.onBeforeBinding = function() {
-            self.r_val = self.settings.settings.plugins.LEDController.red_strength();
-            self.g_val = self.settings.settings.plugins.LEDController.green_strength();
-            self.b_val = self.settings.settings.plugins.LEDController.blue_strength();
-            self.setColor();
+            self.r_val = Number(self.settings.settings.plugins.LEDController.red_strength());
+            self.g_val = Number(self.settings.settings.plugins.LEDController.green_strength());
+            self.b_val = Number(self.settings.settings.plugins.LEDController.blue_strength());
+            self.set_color();
         }
     }
 
     OCTOPRINT_VIEWMODELS.push({
         construct: LEDControllerViewModel,
         dependencies: ['settingsViewModel'],
-        elements: ["#settings_plugin_LEDController"],
+        elements: ["settings_plugin_LEDController"],
     });
 });
